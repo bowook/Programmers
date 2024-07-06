b86ee958497204001b8fa12a16f748d4aed23620
wook <129655108+bowook@users.noreply.github.com>
2024-07-06 20:13:07 +0900

    [Bronze III] Title: 학점계산, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, Double> map = new HashMap<String, Double>();
+        map.put("A+", 4.3);
+        map.put("A0", 4.0);
+        map.put("A-", 3.7);
+        map.put("B+", 3.3);
+        map.put("B0", 3.0);
+        map.put("B-", 2.7);
+        map.put("C+", 2.3);
+        map.put("C0", 2.0);
+        map.put("C-", 1.7);
+        map.put("D+", 1.3);
+        map.put("D0", 1.0);
+        map.put("D-", 0.7);
+        map.put("F", 0.0);
+
+        String key = scanner.nextLine();
+        System.out.println(map.get(key));
+    }
+}
