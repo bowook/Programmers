de54de698d888258ddb57ae65315a2d210b7d3c6
wook <129655108+bowook@users.noreply.github.com>
2024-07-10 09:41:24 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 184 ms, Memory: 18300 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String hongjun = ":"+scanner.nextLine() + ":";
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan:"+hongjun+":fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
