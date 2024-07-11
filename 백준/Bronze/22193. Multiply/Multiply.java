76bc0a19991e68cebde876cda81f4402f3934696
wook <129655108+bowook@users.noreply.github.com>
2024-07-11 19:22:48 +0900

    [Bronze V] Title: Multiply, Time: 760 ms, Memory: 60280 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -1,18 +1,30 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] arr = scanner.nextLine().split(" ");
-        int[] num1 = new int[arr[0].length()];
-        int[] num2 = new int[arr[1].length()];
 
-        num1[0] = scanner.nextInt();
-        scanner.nextLine();
+        // 첫 번째 라인에서 N과 M을 입력받습니다.
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
 
-        num2[0] = scanner.nextInt();
-        scanner.nextLine();
+        // 두 번째 라인에서 A를 입력받습니다.
+        String A = scanner.next();
 
-        System.out.println(num1[0] * num2[0]);
+        // 세 번째 라인에서 B를 입력받습니다.
+        String B = scanner.next();
+
+        // BigInteger를 사용하여 큰 숫자를 처리합니다.
+        BigInteger numA = new BigInteger(A);
+        BigInteger numB = new BigInteger(B);
+
+        // A와 B의 곱을 계산합니다.
+        BigInteger result = numA.multiply(numB);
+
+        // 결과를 출력합니다.
+        System.out.println(result);
+
+        scanner.close();
     }
 }
