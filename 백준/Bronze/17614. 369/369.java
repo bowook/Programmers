7d40c200f1aa227b1dbddfeda4eba31c09854753
wook <129655108+bowook@users.noreply.github.com>
2024-07-12 18:48:07 +0900

    [Bronze III] Title: 369, Time: 136 ms, Memory: 14336 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.java
+++ 백준/Bronze/17614. 369/369.java
@@ -5,23 +5,21 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        String N = br.readLine();
+        int N = Integer.parseInt(br.readLine());
 
         int clapCount = 0;
-        for(int i = 1; i <= Long.parseLong(N); i ++) {
-            String[] arr = String.valueOf(i).split("");
-
-            for(int j = 0; j < arr.length; j ++) {
-
-                if(Integer.parseInt(arr[j]) != 0 && Integer.parseInt(arr[j])% 3 == 0) {
-                    clapCount += 1;
+        for(int i = 1; i <= N; i ++) {
+            int num = i;
+            while(num > 0) {
+                int digit = num % 10;
+                if(digit != 0 && digit % 3 == 0) {
+                    clapCount++;
                 }
+                num /=10;
             }
         }
-        sb.append(clapCount);
-        System.out.println(sb);
+        System.out.println(clapCount);
 
     }
 }
