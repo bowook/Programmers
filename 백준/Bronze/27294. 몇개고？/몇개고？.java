165627f5e2741e682f1a45b6afabca508ce1d6e3
wook <129655108+bowook@users.noreply.github.com>
2024-09-19 23:33:20 +0900

    [Bronze V] Title: 몇개고?, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int t = Integer.parseInt(st.nextToken()); // 시간
+		int s = Integer.parseInt(st.nextToken()); // 술의 유무
+		
+		int result = 0;
+	
+		if (s == 1) {
+			result = 280;
+			
+		} else {
+			
+			if (12 <= t && t <= 16) {
+				result = 320;
+				
+			} else {
+				result = 280;
+			}
+		}
+		
+		bw.write(Integer.toString(result));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
