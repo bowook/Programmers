008506442cdf45715c29859075758813f8853cfa
wook <129655108+bowook@users.noreply.github.com>
2024-10-10 18:17:18 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        br.close();
+        if(L % 5 != 0) {
+            System.out.println(L/5 + 1);
+        }
+        else {
+            System.out.println(L/5);
+        }
+    }
+
+
+}
