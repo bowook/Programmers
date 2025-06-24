2ad2d00b0cca4b2be2872f33956373474fd3450c
wook <mbosx316@naver.com>
2025-06-24 19:24:24 +0900

    [Silver II] Title: 로또, Time: 168 ms, Memory: 15596 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringJoiner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int k = Integer.parseInt(st.nextToken());
+
+            if (k == 0) {
+                break;
+            }
+
+            int[] s = new int[k];
+            for (int i = 0; i < k; i++) {
+                s[i] = Integer.parseInt(st.nextToken());
+            }
+
+            dfs(s, 0, 0, new int[6]);
+            System.out.println();
+        }
+
+    }
+
+    private static void dfs(int[] arr, int start, int depth, int[] result) {
+        if (depth == 6) {
+            StringJoiner sj = new StringJoiner(" ");
+            for (int i = 0; i < 6; i++) {
+                sj.add(String.valueOf(result[i]));
+            }
+            System.out.println(sj);
+            return;
+        }
+
+        for (int i = start; i < arr.length; i++) {
+            result[depth] = arr[i];
+            dfs(arr, i + 1, depth + 1, result);
+        }
+    }
+
+}
