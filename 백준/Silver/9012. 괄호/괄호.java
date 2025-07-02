57f2f15a28ad3f10670f623fa731c4106001d898
wook <mbosx316@naver.com>
2025-07-02 22:35:57 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
     private static final String YES = "YES";
@@ -14,21 +13,8 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < T; i++) {
-            String[] brackets = br.readLine().split("");
-            Stack<String> stack = new Stack<>();
-
-            for (String bracket : brackets) {
-                if (bracket.equals("(")) {
-                    stack.push(bracket);
-                } else if (!stack.isEmpty() && bracket.equals(")")) {
-                    stack.pop();
-                } else if (stack.isEmpty() && bracket.equals(")")) {
-                    stack.push(bracket);
-                    break;
-                }
-            }
-
-            if (stack.isEmpty()) {
+            char[] brackets = br.readLine().toCharArray();
+            if (isVPS(0, brackets, 0)) {
                 sb.append(YES).append("\n");
                 continue;
             }
@@ -36,5 +22,25 @@ public class Main {
         }
 
         System.out.println(sb.toString().trim());
+    }
+
+    private static boolean isVPS(int index, char[] brackets, int count) {
+        if (count < 0) {
+            return false;
+        }
+
+        if (index == brackets.length && count == 0) {
+            //같을 때에 true를 반환해야하는 경우도 있어서 위에서 count <0 과 함께 한번에 false를 처리하면 안 됨.
+            return true;
+        } else if (index == brackets.length) {
+            return false;
+        }
+
+        char bracket = brackets[index];
+
+        if (bracket == '(') {
+            return isVPS(index + 1, brackets, count + 1);
+        }
+        return isVPS(index + 1, brackets, count - 1);
     }
 }
