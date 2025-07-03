123bd8cf33a14bb017a5e45ea00e45d4bbb0dbf7
wook <mbosx316@naver.com>
2025-07-03 21:16:05 +0900

    [Gold V] Title: 암호 만들기, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,72 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static final List<String> consonant = List.of("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p",
-            "q", "r", "s", "t", "v", "w", "x", "y", "z");
-    private static final List<String> vowel = List.of("a", "e", "o", "i", "u");
-    private static final List<String> strings = new ArrayList<>();
-    private static int LENGTH;
+    private static final Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+    private static int L, C;
+    private static char[] word, password;
+    private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        LENGTH = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-        String[] alphabets = br.readLine().split(" ");
-        Arrays.sort(alphabets);
-
-        int temp = C - LENGTH;
-        for (int i = 0; i <= temp; i++) {
-            recursive(0, i, alphabets, new Stack<>());
+        word = new char[C];
+        password = new char[L];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < C; i++) {
+            word[i] = st.nextToken().charAt(0);
         }
+        Arrays.sort(word);
 
-        for (String str : strings) {
-            System.out.println(str);
-        }
+        dfs(0, 0, 0, 0);
+        System.out.print(sb);
     }
 
-    private static void recursive(int count, int index, String[] alphabets, Stack<String> stack) {
-        if (count == LENGTH) {
-            StringBuilder stringBuilder = new StringBuilder();
-            int vowelCount = 0;
-            int consonantCount = 0;
-            for (String s : stack) {
-                if (vowel.contains(s)) {
-                    vowelCount += 1;
-                } else if (consonant.contains(s)) {
-                    consonantCount += 1;
-                }
-                stringBuilder.append(s);
-            }
-
-            if (vowelCount < 1 || consonantCount < 2) {
-                return;
-            }
-
-            if (!strings.contains(stringBuilder.toString())) {
-                strings.add(stringBuilder.toString());
+    static void dfs(int idx, int depth, int v, int c) {
+        if (depth == L) {
+            if (v >= 1 && c >= 2) {
+                sb.append(password).append('\n');
             }
             return;
         }
-
-        if (index >= alphabets.length) {
+        if (idx >= C) {
             return;
         }
 
-        stack.add(alphabets[index]);
-        recursive(count + 1, index + 1, alphabets, stack);
-
-        stack.pop();
-        for (int i = 1; i <= alphabets.length - 1 - index; i++) {
-            recursive(count, index + i, alphabets, stack);
+        // 선택 ㅋㅋㄹㅃㅃ
+        password[depth] = word[idx];
+        if (vowels.contains(word[idx])) {
+            dfs(idx + 1, depth + 1, v + 1, c);
+        } else {
+            dfs(idx + 1, depth + 1, v, c + 1);
         }
+        
+        // 선택 안함 ㅋㄹㅋ
+        dfs(idx + 1, depth, v, c);
     }
 }
