395df3ff8e1978e7e73938b06a5021a3ef08c698
wook <mbosx316@naver.com>
2025-08-10 23:14:25 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,7 +1,8 @@
 class Solution {
     public int solution(final int n) {
         int answer = 0;
-        for (int i = 1; i <= n/2; i ++) {
+        int half = n / 2;
+        for (int i = 1; i <= half; i ++) {
             if (n % i == 0) {
                 answer += i;
             }
