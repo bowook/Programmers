32e9a1b017ba2cd25987d9576ccdc6dad0e82b2f
wook <mbosx316@naver.com>
2025-10-04 18:00:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 85.10 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,6 +4,7 @@ class Solution {
     //완주하지 못한 선수를 반환해야함!
     //동명이인이 있을 수 있음.
     public String solution(final String[] participant, final String[] completion) {
+        String answer = "";
         Map<String, Integer> mapForParticipant = new HashMap<>();
         for (String person : participant) {
             mapForParticipant.put(person, mapForParticipant.getOrDefault(person, 0) + 1);
@@ -13,12 +14,13 @@ class Solution {
             mapForParticipant.put(person, mapForParticipant.get(person)-1);
         }
         
-        for(Map.Entry<String, Integer> entry : mapForParticipant.entrySet()) {
-            if (entry.getValue() > 0) {
-                return entry.getKey();
+        for (String person : mapForParticipant.keySet()) {
+            if (mapForParticipant.get(person) > 0) {
+                answer = person;
+                break;
             }
         }
         
-        return null;
+        return answer;
     }
 }
