07e657fb548f719f08c32f3d4566b66643ef43e4
wook <mbosx316@naver.com>
2025-10-10 20:24:02 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 51.23 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(final int[] arr) {
+        //배열의 크기는 자연수라고 했으니까 0일 수 없음.
+        //배열이 비어있다는 케이스는 존재하지 않음.
         Stack<Integer> stack = new Stack<>();
         stack.push(arr[0]);
         for (int number : arr) {
@@ -11,8 +13,8 @@ public class Solution {
         }
         
         int[] answer = new int[stack.size()];
-        for (int i = 0; i < answer.length; i ++) {
-            answer[i] = stack.get(i);
+        for (int i = stack.size() - 1; i >= 0; i --) {
+            answer[i] = stack.pop();
         }
         
         return answer;
