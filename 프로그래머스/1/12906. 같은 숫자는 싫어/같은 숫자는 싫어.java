9d55a00abf02e3dfec7665d7be6502a7bf8558c9
wook <mbosx316@naver.com>
2025-10-10 20:33:29 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 48.61 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -4,19 +4,15 @@ public class Solution {
     public int[] solution(final int[] arr) {
         //배열의 크기는 자연수라고 했으니까 0일 수 없음.
         //배열이 비어있다는 케이스는 존재하지 않음.
-        Stack<Integer> stack = new Stack<>();
-        stack.push(arr[0]);
-        for (int number : arr) {
-            if (stack.peek() != number) {
-                stack.push(number);
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.addFirst(arr[0]);
+        
+        for(int number : arr) {
+            if (deque.peekLast() != number) {
+                deque.addLast(number);
             }
         }
         
-        int[] answer = new int[stack.size()];
-        for (int i = stack.size() - 1; i >= 0; i --) {
-            answer[i] = stack.pop();
-        }
-        
-        return answer;
+        return deque.stream().mapToInt(Integer::intValue).toArray();
     }
 }
