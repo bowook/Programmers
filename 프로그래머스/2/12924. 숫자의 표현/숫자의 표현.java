b3d4f1a2c49fb3668e46d72a7152820de04cd48c
wook <mbosx316@naver.com>
2025-10-27 17:05:20 +0900

    [level 2] Title: 숫자의 표현, Time: 0.11 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,18 +1,10 @@
 class Solution {
     public int solution(int n) {
         int count = 0;
-        
-        for(int i = 1; i <= n; i ++) {
-            int sum = 0;
-            for(int j = i; j <= n; j ++) {
-                sum += j;
-                if (sum == n) {
-                    count += 1; 
-                    break;
-                }
-                if (sum > n) {
-                    break;
-                }
+    
+        for(int i = 1; i <= n; i += 2) {
+            if (n % i == 0) {
+                count += 1;
             }
         }
         
