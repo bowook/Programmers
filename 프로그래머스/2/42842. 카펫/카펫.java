804e107230ec9d5e7465c8f0a4818cd2c604f998
wook <mbosx316@naver.com>
2025-10-28 22:18:01 +0900

    [level 2] Title: 카펫, Time: 8.73 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int carpetSize = brown + yellow;
+        
+        Deque<Integer> deque = findWidthAndHeight(carpetSize);
+        while (true) {
+            int width = deque.pollFirst();
+            int height = deque.pollFirst();
+            
+            if((width - 2) * (height - 2) == yellow) {
+                answer[0] = width;
+                answer[1] = height;
+                break;
+            }
+        }
+        
+        int temp = 0;
+        if (answer[1] > answer[0]) {
+            temp = answer[0];
+            answer[0] = answer[1];
+            answer[1] = temp;
+        }
+        
+        return answer;
+    }
+    
+    private Deque<Integer> findWidthAndHeight(int size) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        int height = 3;
+        while(true) {
+            if (height >= size) {
+                break;
+            }
+
+            if (size % height == 0) {
+                int width = size / height;
+                deque.addLast(width);
+                deque.addLast(height);
+            }
+            height ++;
+        }
+        
+        return deque;
+    }
+}
