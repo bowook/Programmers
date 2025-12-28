2b29f1f8cfa1ede1777b05db94777a6d250b4f02
wook <mbosx316@naver.com>
2025-12-28 21:49:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 124 ms, Memory: 16800 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,48 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    //최단 거리를 보장해야함, dfs X -> bfs 사용
-    private static int K = 0;
-    private static boolean[] visited = new boolean[100001];
-
+    
+    private static final int maxCol = 100001;
+    
+    private static boolean[] visited;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken()); //N이 수빈이의 현재 점
-        K = Integer.parseInt(st.nextToken()); //K가 동생의 현재 점
-
-        System.out.println(bfs(N));
+        String[] startLine = br.readLine().split(" ");
+        int N = Integer.parseInt(startLine[0]);
+        int K = Integer.parseInt(startLine[1]);
+        visited = new boolean[100001];
+        
+        bfs(N, K);
     }
-
-    private static int bfs(int currentCoordinate) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{currentCoordinate, 0});
-
-        while (!queue.isEmpty()) {
-            int[] pollValue = queue.poll();
-            int position = pollValue[0];
-            int time = pollValue[1];
-
-            visited[position] = true;
-
-            if (position == K) {
-                //이때 시간을 리턴해줘야하는데 음.. 아 이렇게 해볼까
-                //큐에다가 그냥 좌표랑 시간을 같이 넣을까 음..
-                return time;
+    
+    private static void bfs(int start, int target) {
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.addLast(new int[]{start,0});
+        visited[start] = true;
+        
+        while (!deque.isEmpty()) {
+            int[] current = deque.pollFirst();
+            int currentPos = current[0];
+            int currentMoves = current[1];
+            
+            if (currentPos == target) {
+                System.out.println(currentMoves);
+                return;
             }
-
-            int[] possibleMoveCoordinate = new int[] {position+1,position-1,position*2};
-
-            for(int move : possibleMoveCoordinate) {
-                if(move >=0 && move <= 100000 && !visited[move]) {
-                    queue.offer(new int[]{move, time+1});
-                }
+            
+            int plusWalk = currentPos + 1;
+            int minusWalk = currentPos - 1;
+            int multiplyWalk = currentPos * 2;
+            
+            if (plusWalk >= 0 && plusWalk < maxCol && !visited[plusWalk]) {
+                deque.addLast(new int[]{plusWalk, currentMoves + 1});
+                visited[plusWalk] = true;
+            }
+            if (minusWalk >= 0 && minusWalk < maxCol && !visited[minusWalk]) {
+                deque.addLast(new int[]{minusWalk, currentMoves + 1});
+                visited[minusWalk] = true;
+            }
+            if (multiplyWalk >= 0 && multiplyWalk < maxCol && !visited[multiplyWalk]) {
+                deque.addLast(new int[]{multiplyWalk, currentMoves + 1});
+                visited[multiplyWalk] = true;
             }
         }
-        return -1;
     }
 }
