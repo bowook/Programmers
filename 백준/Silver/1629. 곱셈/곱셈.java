69275d0e3ea2a93796c9c28e0490f1d38a6af2e9
wook <mbosx316@naver.com>
2025-12-30 18:51:25 +0900

    [Silver I] Title: 곱셈, Time: 96 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,52 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    //    10 11 12
-    //11홀수니까
-    //10 * (10 ^(11-1)/2) ^ 2 -> 10 * ( 10 ^ 5) ^ 2
-    //10 5 12
-    //5는 홀수니까
-    //10 * (10 ^ (5-1)/2) ^2 -> 10 * (10 ^ 2) ^ 2
-    //10 2 12
-    //2는 짝수니까
-    //(10 ^ 1 ) ^ 2
-    //10 1 12
-    //1은 홀수니까
-    //10 * ( 10 ^ (1-1)/2) ^ 2 -> 10 * (10 ^ 0 ) ^ 2
-    //exp == 0이 되면
-    //return 1;
-    //그러면
-    //10 * 1 ^ 2 = 10 % 12 = 10
-    //10 ^ 2 = 100 % 12 = 4
-    //10 * 16 = 160 % 12 = 4
-    //10 * 16 = 160 % 12 = 4
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int C = Integer.parseInt(st.nextToken());
-        int result = modularExponential(A,B,C);
+        
+        long result = calculate(A, B, C);
         System.out.println(result);
     }
-    private static int modularExponential(int base, int exp, int mod) {
-        if(exp == 0) {
+    
+    private static long calculate(long a, long b, long c) {
+        if (b == 0) {
             return 1;
         }
-
-        base %= mod;
-
-        if(exp % 2 == 0) {
-            // 지수가 짝수인 경우
-            int half = modularExponential(base, exp / 2, mod);
-            return (int)((long)half * half % mod);
-        } else {
-            // 지수가 홀수인 경우
-            int half = modularExponential(base, (exp - 1) / 2, mod);
-            return (int)((long)half * half % mod * base % mod);
+        long half = calculate(a, b/2, c);
+        long temp = (half * half) % c;
+        if (b % 2 == 0) {
+            return temp;
+        }
+        else {
+            return ((a % c) * temp) % c;
         }
     }
 }
