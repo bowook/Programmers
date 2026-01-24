527edb822417c8742b3ad30ffb951aa3eedf621b
wook <mbosx316@naver.com>
2026-01-24 16:59:13 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,42 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
-            HashMap<String, List<String>> map = new HashMap<>();
-
-            for (int j = 0; j < N; j++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                String part = st.nextToken(); // 의상의 이름
-                String type = st.nextToken(); // 의상의 종류
-
-                // 의상의 종류에 따라 리스트에 추가
-                map.computeIfAbsent(type, k -> new ArrayList<>()).add(part);
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i ++) {
+            int value = Integer.parseInt(br.readLine());
+            Map<String, Integer> closet = new HashMap<>();
+            for (int j = 0; j < value; j ++) {
+                String[] nameAndType = br.readLine().split(" ");
+                String name = nameAndType[0];
+                String type = nameAndType[1];
+                closet.put(type, closet.getOrDefault(type, 0) + 1);
             }
 
-            int combinations = 1; // 기본값 1로 시작
-            for (String key : map.keySet()) {
-                int listSize = map.get(key).size();
-                combinations *= (listSize + 1); // 각 종류에서 선택할 수 있는 경우의 수
+            int sum = 1;
+            for (int count : closet.values()) {
+                sum *= (count + 1);
             }
 
-            // 아무 의상도 입지 않는 경우를 제외
-            combinations -= 1;
-
-            System.out.println(combinations);
-
-            map.clear();
+            System.out.println(sum - 1);
         }
     }
 }
+
