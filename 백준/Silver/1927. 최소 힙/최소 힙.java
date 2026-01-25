aab5aba866cf12b788c904dd0d6f69a981359d39
wook <mbosx316@naver.com>
2026-01-25 21:48:02 +0900

    [Silver II] Title: 최소 힙, Time: 292 ms, Memory: 27340 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,29 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         int N = Integer.parseInt(br.readLine());
-
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for(int i = 0; i < N; i ++) {
-            int inputNumber = Integer.parseInt(br.readLine());
-
-            if(inputNumber == 0) {
-                if(minHeap.isEmpty()) {
-                    System.out.println(0);
-                }
-                else {
-                    System.out.println(minHeap.poll());
-                }
-            }
-            else {
-                minHeap.add(inputNumber);
+        for (int i = 0; i < N;  i ++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x != 0) {
+                pq.add(x);
+                continue;
             }
 
+            Integer value = pq.poll();
+            if (value == null) {
+                value = 0;
+            }
+            sb.append(value)
+                .append("\n");
         }
+
+        System.out.println(sb);
     }
 }
